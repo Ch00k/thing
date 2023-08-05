c4f7282cf68ae6ff932c9b45fc1f82332ca2944d
Andrii Yurchuk <ay@mntw.re>
2023-08-05 07:00:17 +0200

    Switch to MySQL (#3)

--- src/test/kotlin/my/thingservice/thing/RepositoriesTests.kt
+++ src/test/kotlin/my/thingservice/thing/RepositoriesTests.kt
@@ -3,11 +3,14 @@ package my.thingservice.thing
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
 import org.springframework.data.repository.findByIdOrNull
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
 class RepositoriesTests
 @Autowired
 constructor(
